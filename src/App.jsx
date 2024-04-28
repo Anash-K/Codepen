4f3f90fd11595ccc8ec5be2b54109d8ed8c91655
Anash-K <anashkhan5777@gmail.com>
2024-04-28 10:34:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './Components/home'
+
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Home/>
+      <h1>this is heading</h1>
+      <Routes>
+        <Route />
+      </Routes>
+    </>
+  )
+}
+
+export default App
